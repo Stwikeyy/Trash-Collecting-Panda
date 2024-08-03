619fa19b29ec04df8b183e2eb6f4f5dd6115fd6c
thunderfern <xyfernx@gmail.com>
2024-08-03 14:53:37 -0400

    did trash generation

---
+++ Save the Turtles (HT6)/Assets/Scripts/TrashGenerator.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashGenerator : MonoBehaviour {
+    
+    public GameObject trash;
+    public int num;
+    public int trashGenerated;
+
+    // Start is called before the first frame update
+    
+
+    void Start() {
+        createTrash(10);
+    }
+
+    void Update() {
+        if (num < 1000) num++;
+        if (num >= 1000 && trashGenerated <= 15) {
+            createTrash(1);
+            num = 0;
+            trashGenerated++;
+        }
+    }
+
+    void createTrash(int num) {
+        for (int i = 0; i < num; i++) {
+            int xpos = Random.Range(-10, 10), ypos = Random.Range(-5, 5);
+            GameObject trashClone = Instantiate(trash, new Vector3(xpos, ypos, 0), trash.transform.rotation);
+            trashClone.layer = 0;
+        }
+    }
+}
