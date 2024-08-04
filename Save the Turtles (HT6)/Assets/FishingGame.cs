d6a654c9f2560258e4979929dd8b8d65d609b536
Mintches <70064747+Mintches@users.noreply.github.com>
2024-08-03 20:03:20 -0400

    Merge

--- Save the Turtles (HT6)/Assets/FishingGame.cs
+++ Save the Turtles (HT6)/Assets/FishingGame.cs
@@ -4,6 +4,31 @@ using UnityEngine;
 
 public class FishingGame : MonoBehaviour
 {
+    [SerializeField] Transform topPivot;
+    [SerializeField] Transform bottomPivot;
+
+    [SerializeField] Transform fish;
+
+    float fishPosition;
+    float fishDestination;
+
+    float fishTimer;
+    [SerializeField] float timerMultiplicator = 3f;
+
+    float fishSpeed;
+    [SerializeField] float smoothMotion = 1f;
+
+    [SerializeField] Transform hook;
+    float hookPosition;
+    [SerializeField] float hookSize = 0.1f;
+    [SerializeField] float hookPower = 0.5f;
+    float hookProgress;
+    float hookPullVelocity;
+    [SerializeField] float hookPullPower = 0.01f;
+    [SerializeField] float hookGravityPower = 0.005f;
+    [SerializeField] float hookProgressDegradationPower = 0.1f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +36,34 @@ public class FishingGame : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update() {
+        Fish();
+        Hook();
+    }
+
+    void Hook() {
+        if (Input.GetButtonDown("Jump")) {
+            hookPullVelocity += hookPullPower * Time.deltaTime;
+        }
+        hookPullVelocity -= hookGravityPower * Time.deltaTime;
+
+        hookPosition += hookPullVelocity;
+        hookPosition = Mathf.Clamp(hookPosition, 0, 1);
+        hook.position = Vector2.Lerp(bottomPivot.position, topPivot.position, hookPosition);
+    }
+
+    void Fish()
     {
-        
+        // shows up when fishing
+
+        fishTimer -= Time.deltaTime;
+        if (fishTimer < 0f) {
+            fishTimer = UnityEngine.Random.value * timerMultiplicator;
+            fishDestination = UnityEngine.Random.value;
+        }
+
+
+        fishPosition = Mathf.SmoothDamp(fishPosition, fishDestination, ref fishSpeed, smoothMotion);
+        fish.position = Vector2.Lerp(bottomPivot.position,topPivot.position, fishPosition);
     }
 }
